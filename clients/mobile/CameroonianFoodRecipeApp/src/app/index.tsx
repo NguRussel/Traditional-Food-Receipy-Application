d1b1bae64f1c9fbe9bc1723d8a307de1a16678d1
Ghislain Che Ngwateh <ghislain.ngwateh@ictuniversity.edu.cm>
2025-05-28 23:24:33 +0100

    Expo router installed

--- clients/mobile/CameroonianFoodRecipeApp/src/app/index.tsx
+++ clients/mobile/CameroonianFoodRecipeApp/src/app/index.tsx
@@ -3,8 +3,8 @@ import { StyleSheet,
   Text, KeyboardAvoidingView,
   Platform, View
  } from 'react-native';  
- import CustomInput from './src/components/CustomInput';
-import CustomButton from './src/components/CustomButton';
+ import CustomInput from '@/components/CustomInput';
+import CustomButton from '@/components/CustomButton';
 import {useForm} from 'react-hook-form';
 import {z} from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod';
